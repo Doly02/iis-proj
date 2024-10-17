24b3e757390c7c7cdd3ad6cb6d1bcec5c675b153
Tomáš Dolák <tomas.dolak@outlook.cz>
2024-10-17 12:13:56 +0200

    [User/Authentification] Adds Baseline for User Module. Updates .gitignore and .neon Files.

--- src/app/Core/RouterFactory.php
+++ src/app/Core/RouterFactory.php
@@ -6,16 +6,23 @@ namespace App\Core;
 
 use Nette;
 use Nette\Application\Routers\RouteList;
-
+use Tracy\Debugger;
 
 final class RouterFactory
 {
-	use Nette\StaticClass;
+    use Nette\StaticClass;
 
-	public static function createRouter(): RouteList
-	{
-		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
-		return $router;
-	}
+    public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        $router->addRoute('', 'Home:default');
+
+        $router[] = new \Nette\Application\Routers\Route('[<module>[/<presenter>[/<action>[/<id \d+>]]]]', [
+            'module' => 'Core',
+            'presenter' => 'Homepage',
+            'action' => 'default',
+        ]);
+
+        return $router;
+    }
 }
