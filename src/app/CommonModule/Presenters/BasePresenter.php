a02475b8673a5a8face1beda17929246b3f30b9c
xzahra33 <xzahra33@vutbr.cz>
2024-10-21 22:51:11 +0200

    [Common] Edit Base Presenter 2.0

--- src/app/CommonModule/Presenters/BasePresenter.php
+++ src/app/CommonModule/Presenters/BasePresenter.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace App\CommonModule\Presenters;
 
 use Nette\Application\UI\Presenter;
+use Nette\Database\Explorer; // Importing the correct class
 
 abstract class BasePresenter extends Presenter
 {
-    public function __construct(
-        private Nette\Database\Explorer $database
-    )
+    private Explorer $database;
+
+    public function __construct(Explorer $database)
     {
         parent::__construct(); // Calling parent constructor
+        $this->database = $database;
     }
+
 
     public function beforeRender(): void
     {
