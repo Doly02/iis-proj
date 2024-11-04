5f8722cd3fda7f17d57080a5d2ed0151301c6bab
xkruma01 <xkruma01@vutbr.cz>
2024-11-04 22:15:32 +0100

    [CONFERENCE] Displaying buttons for edit, add room and delete just for organizer

--- src/app/ConferenceModule/Presenters/ConferenceDetailPresenter.php
+++ src/app/ConferenceModule/Presenters/ConferenceDetailPresenter.php
@@ -6,22 +6,31 @@ use App\CommonModule\Presenters\BasePresenter;
 use App\ConferenceModule\Controls\DetailConference\DetailConferenceControl;
 use App\ConferenceModule\Controls\DetailConference\IDetailConferenceControlFactory;
 use App\ConferenceModule\Model\ConferenceService;
+use Nette\Security\User;
 
 final class ConferenceDetailPresenter extends BasePresenter
 {
     private $conferenceService;
+    private $user;
 
-    public function __construct(ConferenceService $conferenceService)
+    public function __construct(ConferenceService $conferenceService, User $user)
     {
         $this->conferenceService = $conferenceService;
+        $this->user = $user;
     }
 
     public function renderDefault(int $id): void
     {
         $conference = $this->conferenceService->getConferenceById($id);
+        $userId = $this->user->getId();
+        $organiserId = $conference->id;
 
         $start_time_formatted = date('d.m.Y H:i', strtotime($conference->start_time));
         $end_time_formatted = date('d.m.Y H:i', strtotime($conference->end_time));
+
+        // Pass userId and organizerId to display buttons
+        $this->template->userId = $userId;
+        $this->template->organiserId = $organiserId;
 
         // Pass both the conference and formatted times to the template
         $this->template->conference = $conference;
