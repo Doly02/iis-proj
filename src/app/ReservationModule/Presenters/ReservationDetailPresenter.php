a21abf52e4b7fcd6e4c4c6c21311aed27d9b9d8d
xkruma01 <xkruma01@vutbr.cz>
2024-11-07 15:16:42 +0100

    [TICKETS] Displaying tickets from reservation list

--- src/app/ReservationModule/Presenters/ReservationDetailPresenter.php
+++ src/app/ReservationModule/Presenters/ReservationDetailPresenter.php
@@ -3,41 +3,43 @@
 namespace App\ReservationModule\Presenters;
 
 use App\CommonModule\Presenters\BasePresenter;
-use App\ReservationModule\Controls\DetailReservation\DetailReservationControl;
-use App\ReservationModule\Controls\DetailReservation\IDetailReservationControlFactory;
+use App\ConferenceModule\Model\ConferenceService;
 use App\ReservationModule\Model\ReservationService;
+use App\TicketModule\Model\TicketService;
 use Nette\Security\User;
 
 final class ReservationDetailPresenter extends BasePresenter
 {
     private $reservationService;
     private $user;
+    private $ticketService;
+    private $conferenceService;
 
-    private $detailReservationControlFactory;
 
-    public function __construct(ReservationService $reservationService, User $user, IDetailReservationControlFactory $detailReservationControlFactory)
+    public function __construct(ReservationService $reservationService, User $user, TicketService $ticketService,
+                                ConferenceService $conferenceService)
     {
         parent::__construct();
         $this->reservationService = $reservationService;
         $this->user = $user;
-        $this->detailReservationControlFactory = $detailReservationControlFactory;
+        $this->ticketService = $ticketService;
+        $this->conferenceService = $conferenceService;
     }
 
-    protected function createComponentReservationDetail(): DetailReservationControl
-    {
-        $control = $this->detailReservationControlFactory->create();
-
-        $reservationId = (int) $this->getParameter('id');
-        $control->setReservationId($reservationId);
-
-        return $control;
-    }
-
-    public function actionDefault(): void
+    public function actionDefault(int $id): void
     {
         if (!$this->user->isLoggedIn()) {
             $this->redirect(':CommonModule:Login:default');
         }
+
+        $reservationId = $id;
+
+        $reservation = $this->reservationService->fetchById($reservationId);
+
+        $this->template->reservation = $reservation;
+        $this->template->conference = $this->conferenceService->fetchById($reservation->conference_id);
+        $this->template->reservationId = $reservationId;
+        $this->template->tickets = $this->ticketService->getTicketsForPaidReservation($reservationId);
     }
 
 }
