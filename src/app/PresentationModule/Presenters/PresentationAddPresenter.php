7d8d000e238430188ce24c4df36364a2aa3de00d
xkruma01 <xkruma01@vutbr.cz>
2024-11-09 22:11:15 +0100

    [PRESENTATION] Button for requesting presentation, functional presentation request for a conference

--- src/app/PresentationModule/Presenters/PresentationAddPresenter.php
+++ src/app/PresentationModule/Presenters/PresentationAddPresenter.php
@@ -6,16 +6,30 @@ use App\CommonModule\Presenters\BasePresenter;
 use App\PresentationModule\Model\PresentationService;
 use Nette\Application\UI\Form;
 use Nette\Application\AbortException;
+use Nette\Security\User;
+use Tracy\Debugger;
 
 final class PresentationAddPresenter extends BasePresenter
 {
 
     private PresentationService $presentationService;
+    private User $user;
+    private int $conferenceId;
 
-    public function __construct(PresentationService $presentationService)
+    public function __construct(PresentationService $presentationService, User $user)
     {
         parent::__construct();
         $this->presentationService = $presentationService;
+        $this->user = $user;
+    }
+
+    public function actionAdd(int $id): void
+    {
+        if (!$this->user->isLoggedIn()) {
+            $this->redirect(':CommonModule:Login:default');
+        }
+        $this->conferenceId = $id;
+        Debugger::barDump($this->conferenceId);
     }
 
     protected function createComponentAddPresentationForm(): Form
@@ -38,14 +52,16 @@ final class PresentationAddPresenter extends BasePresenter
 
     public function addPresentationFormSucceeded(Form $form, \stdClass $values): void
     {
+        $userId = $this->user->getId();
         try {
             // Insert the room data into the database
             $this->presentationService->addPresentation([
                 'name' => $values->name,
                 'description' => $values->description,
                 'attachment' => $values->attachment,
-                'conference_id' => 1, // TODO: passed from conference detail
-                'lecturer_id' => 1, // TODO: User id
+                'conference_id' => $this->conferenceId,
+                'lecturer_id' => $userId,
+                'state' => 'waiting'
             ]);
 
             $this->flashMessage('Presentation added successfully.', 'success');
@@ -56,7 +72,8 @@ final class PresentationAddPresenter extends BasePresenter
         } catch (AbortException $e) {
             // Allow AbortException silently (no action needed)
         } catch (\Exception $e) {
-            $form->addError('An error occurred while adding the room: ' . $e->getMessage());
+            $form->addError('An error occurred while requesting presentation: ' . $e->getMessage());
         }
+        $this->redirect(':ConferenceModule:ConferenceList:list');
     }
 }
