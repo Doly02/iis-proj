437a4c289a8aebb5a8823e16e8a101d7a3e1e22f
xkruma01 <xkruma01@vutbr.cz>
2024-11-20 12:26:19 +0100

    [SCHEDULE] Personal schedule - selection form has lecture id

--- src/app/LectureModule/Controls/PersonalSchedule/PersonalScheduleControl.php
+++ src/app/LectureModule/Controls/PersonalSchedule/PersonalScheduleControl.php
@@ -3,6 +3,7 @@
 namespace App\LectureModule\Controls\PersonalSchedule;
 
 use Nette\Application\UI\Control;
+use Nette\Application\UI\Form;
 use Nette\Security\User;
 
 final class PersonalScheduleControl extends Control
@@ -38,5 +39,33 @@ final class PersonalScheduleControl extends Control
         $this->template->scheduleItems = $this->scheduleItems;
 
         $this->template->render();
+    }
+
+    protected function createComponentScheduleForm(): Form
+    {
+        $form = new Form;
+
+        foreach ($this->scheduleItems as $item) {
+            $form->addCheckbox("lecture_{$item['id']}", "select");
+        }
+
+        $form->addSubmit('submit', 'Submit');
+
+        $form->onSuccess[] = [$this, 'processScheduleForm'];
+
+        return $form;
+    }
+
+    public function processScheduleForm(Form $form, \stdClass $values): void
+    {
+        $selectedLectures = [];
+        foreach ($values as $key => $value) {
+            if ($value) {
+                $lectureId = str_replace('lecture_', '', $key);
+                $selectedLectures[] = $lectureId;
+            }
+        }
+
+        bdump($selectedLectures, 'Selected Lectures');
     }
 }
